c3c85583777986c29a527035b4b78f92b916687f
liyiming90 <liyiming90@163.com>
2013-10-24 22:30:24 +0800

    searchdr-liyiming

--- MedicalCase/src/MedicalCase/settings.py
+++ MedicalCase/src/MedicalCase/settings.py
@@ -75,7 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "D:/MedicalCase/MedicalCase/src/resources"
+    "D:/MedicalCase/MedicalCase/src/templates"
 )
 
 # List of finder classes that know how to find static files in
--- MedicalCase/src/TCM/views.py
+++ MedicalCase/src/TCM/views.py
@@ -11,20 +11,23 @@ import datetime
 import MySQLdb
 
 def name(request):
-    doctorname = request.REQUEST.get('doctorname',None)
+    doctorname = request.REQUEST.get('keyword',None)
     other = request.REQUEST.get('other',None)
    
     if doctorname is not None:
         doctorsList=Doctor.objects.all()
+        
         for doctors in doctorsList:
-            if doctors.drname == doctorname:
-                return render_to_response('test.html', {'doctorname': doctors})
+            if doctors.drname == doctorname :
+                return render_to_response('test4.html', {'name': doctors.drname,'intro':doctors.drintroduction})
+                
    
     if other is not None:
         others = Medicalcase.objects.filter(Q(casename__icontains=other)|Q(diagnosis__icontains=other)|Q(therapy__icontains=other)|Q(discrimination__icontains=other))
-        return render_to_response('test.html', {'other': others})
+        return render_to_response('test5.html', {'other': others})
+    return render_to_response('test3.html')
             
-    return render_to_response('test4.html')
+    #return render_to_response('test.html')
     #info= Doctor.objects.get(drname=doctorname)
     #print doctorname
     #return render_to_response('test.html', {'doctorname': info})
